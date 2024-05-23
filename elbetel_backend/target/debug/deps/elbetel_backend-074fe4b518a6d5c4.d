2980b7351d3e01024a520941ab0f561810242246
mathrews <mateusps4gg@gmail.com>
2024-05-23 16:28:58 -0300

    initiating project

---
+++ elbetel_backend/target/debug/deps/elbetel_backend-074fe4b518a6d5c4.d
@@ -0,0 +1,5 @@
+/home/matedev/My-projects/el-betel-backend/elbetel_backend/target/debug/deps/elbetel_backend-074fe4b518a6d5c4: src/main.rs
+
+/home/matedev/My-projects/el-betel-backend/elbetel_backend/target/debug/deps/elbetel_backend-074fe4b518a6d5c4.d: src/main.rs
+
+src/main.rs:
---
+++ elbetel_backend/target/debug/elbetel_backend.d
@@ -0,0 +1,1 @@
+/home/matedev/My-projects/el-betel-backend/elbetel_backend/target/debug/elbetel_backend: /home/matedev/My-projects/el-betel-backend/elbetel_backend/src/main.rs
