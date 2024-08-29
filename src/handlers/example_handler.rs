208164318775ca642b8a61a9913b0cf45caf332b
mathrews <mateusps4gg@gmail.com>
2024-08-29 15:55:29 -0300

    cleaning the code before loss 70% of my work T-T

--- src/handlers/example_handler.rs
+++ src/handlers/example_handler.rs
@@ -1,6 +1,5 @@
-use core::num;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{HttpResponse, Responder};
 
 use crate::services::example_service::give_a_number;
 
